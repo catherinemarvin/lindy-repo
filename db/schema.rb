84d7a0741b28f9717bfb7fcf962042845ccd0969
Catherine Hwang <catherinemhwang@gmail.com>
2017-11-27 20:24:41 -0800

    Actually run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125161426) do
+ActiveRecord::Schema.define(version: 20171128042348) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20171125161426) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "start"
+    t.integer "end"
   end
 
 end
