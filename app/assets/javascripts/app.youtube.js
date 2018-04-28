8ced598c0a907f89f0c74053a2f00f9dade7fab0
Catherine Hwang <catherinemhwang@gmail.com>
2018-04-28 02:26:55 -0700

    Add rudimentary namespacing & basic YouTube JS widget

---
+++ app/assets/javascripts/app.youtube.js
@@ -0,0 +1,45 @@
+App.YouTube = class {
+  constructor(element) {
+    this.element = element;
+  }
+
+  render() {
+    var tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
+}
+
+$(document).ready(function() {
+  var youtube = new App.YouTube($('#player'))
+  youtube.render();
+});
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,8 @@
+window.App = window.App || {}
+
+App.init = function () {};
+
+$(document).ready(function() {
+  App.init();
+});
+
