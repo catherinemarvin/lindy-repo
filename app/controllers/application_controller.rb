1709830d2deef6f5c18b8d34485a373475515ef3
Catherine Hwang <catherinemhwang@gmail.com>
2018-04-30 22:02:46 -0700

    Add helper to remember to check authorization/policy scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
   protect_from_forgery with: :exception
 end
