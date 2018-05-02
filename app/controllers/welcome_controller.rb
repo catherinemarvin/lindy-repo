08e06860ca9b339e94855c77fca72a6bd8ded3f5
Catherine Hwang <catherinemhwang@gmail.com>
2018-05-01 21:32:54 -0700

    Add rudimentary about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     skip_policy_scope
   end
+
+  def about
+    skip_authorization
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :videos
   devise_for :users
   root to: 'welcome#index'
+  get 'about', to: 'welcome#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
